b8a2d4ef25c43d1b8a4f97a56307c302a5847ac3
Aksana Lissikh <quenchelm@gmail.com>
2018-09-15 16:44:28 +0300

    Add brackets check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brackets = str.split('');
+  const stack = [];
+
+  for (let i = 0; i < brackets.length; i++) {
+    lastStackValue = stack[stack.length - 1];
+    if (isOpening(brackets[i], bracketsConfig) && !isTheSame(lastStackValue, brackets[i], bracketsConfig)) {
+      stack.push(brackets[i]);
+    } else {
+      if (stack.length === 0 || (stack.pop() !== getMatchingOpening(brackets[i], bracketsConfig))) { return false; }
+    }
+  }
+  return stack.length === 0;
+}
+
+function getMatchingOpening(closing, bracketsConfig) {
+  return bracketsConfig.find(brackets => closing === brackets[1])[0];
+}
+
+function isOpening(currentValue, bracketsConfig) {
+  return bracketsConfig.find(brackets => currentValue === brackets[0]);
+}
+
+function isTheSame(lastStackValue, currentValue, bracketsConfig) {
+  return bracketsConfig.find(brackets => lastStackValue === brackets[0] && currentValue === brackets[1]);
 }
